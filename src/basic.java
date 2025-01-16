3b495616a2f303ba2049231251ee0cfcb6049162
MuskanVishwakarmaa <muskanvishwakarma06941@gmail.com>
2025-01-16 16:18:01 +0530

    Initial

---
+++ src/basic.java
@@ -0,0 +1,26 @@
+import java.sql.SQLOutput;
+import java.util.*;
+public class basic {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println(enter number: );
+        int choice = sc.nextInt();
+        switch (choice){
+            case 1:
+                System.out.println(a+b);
+                break:
+            case 2:
+                System.out.println(a-b);
+                break:
+            case 3:
+                System.out.print.ln(a*b);
+                break:
+            case 4:
+                System.out.println(a/b);
+                break:
+
+        }
+    }
+}
